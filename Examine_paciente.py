bc0551a035b70c4883f8064edabaca4ac264791a
Marcos Geovane Araújo Santos <111813023+marcosgeovanee@users.noreply.github.com>
2025-08-09 16:35:38 -0300

    Add files via upload

---
+++ Examine_paciente.py
@@ -0,0 +1,18 @@
+temperatura = float(input())
+resposta = input()
+
+if resposta != "S" and resposta != "N":
+    print("Erro")
+    
+elif temperatura < 35 or temperatura > 41:
+    print("Erro")
+
+else:
+    if temperatura >= 37 and resposta == "S":
+        print("Exames Especiais")
+    elif temperatura >= 37 and resposta == "N":
+        print("Exames Basicos")
+    elif temperatura < 37 and resposta == "N":
+        print("Liberado")
+    elif temperatura < 37 and resposta == "S":
+        print("Exames Basicos")
